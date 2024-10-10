c550d4500e2838231fca28b259c1ab850cd4d6d3
Juan Carlos Martínez Díaz <jcmartinezd@unal.edu.co>
2024-10-09 19:03:03 -0500

    reparacion

--- servidor.py
+++ servidor.py
@@ -1,24 +1,20 @@
-from flask import Flask, jsonify# Importa Flask para crear el servidor y jsonify para formatear las respuestas como JSON
+from flask import Flask, jsonify  # Importa Flask para crear el servidor y jsonify para formatear las respuestas como JSON
 
-app = Flask(__name__)
+app = Flask(__name__)  # Inicializa una aplicación Flask
 
 # Base de datos simulada
 base_datos = {
     "usuarios": [
         {"id": 1, "nombre": "Juan"},
         {"id": 2, "nombre": "María"}
-        {"id": 3, "nombre": "manuel"}
-        {"id": 4, "nombre": "Alexa"}
-        {"id": 5, "nombre": "Kevin"}
-        {"id": 6, "nombre": "Dario"}
-        {"id": 7, "nombre": "Andres"}
-        {"id": 8, "nombre": "Stivel"}
     ]
 }
+
 # Ruta para obtener los usuarios
 @app.route('/usuarios', methods=['GET'])
 def obtener_usuarios():
-    return jsonify(base_datos["usuarios"]) # Devuelve la lista de usuarios en formato 
-JSON
-app.run(port=5000)
+    return jsonify(base_datos["usuarios"])  # Devuelve la lista de usuarios en formato JSON
+
+if __name__ == '__main__':
+    app.run(port=5000)  # Ejecuta el servidor en el puerto 5000
 
